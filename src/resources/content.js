c5bc0b733701e18b83f01b31d6790c6edefa11f3
Danil <danilhendrasr@gmail.com>
2025-07-28 19:59:26 +0700

    chore: change tab icon

--- src/resources/content.js
+++ src/resources/content.js
@@ -51,7 +51,7 @@ const social = [
 
 const home = {
   path: "/",
-  image: "/images/og/home.jpg",
+  image: "/images/portrait.jpeg",
   label: "Home",
   title: `${person.name}'s Portfolio`,
   description: `Portfolio website showcasing my work as a ${person.role}`,
